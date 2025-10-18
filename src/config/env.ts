1e8c0699e1ef47cda571a2597dbe2ece5eae5451
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-10-18 23:40:19 +0600

    Admin creds added to env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,10 +3,22 @@ dotenv.config();
 
 interface Env {
   PORT: number;
+  NODE_ENV: string;
+  Admin: {
+    name: string;
+    email: string;
+    password: string;
+  };
 }
 
 function loadEnv(): Env {
-  const requiredVariables: string[] = ["PORT"];
+  const requiredVariables: string[] = [
+    "PORT",
+    "NODE_ENV",
+    "ADMIN_NAME",
+    "ADMIN_EMAIL",
+    "ADMIN_PASSWORD",
+  ];
   requiredVariables.forEach((variable) => {
     if (!process.env[variable]) {
       throw new Error(`Missing environment variable: ${variable}`);
@@ -15,6 +27,14 @@ function loadEnv(): Env {
   return {
     // Server
     PORT: Number(process.env.PORT),
+    NODE_ENV: process.env.NODE_ENV as string,
+
+    // Admin
+    Admin: {
+      name: process.env.ADMIN_NAME as string,
+      email: process.env.ADMIN_EMAIL as string,
+      password: process.env.ADMIN_PASSWORD as string,
+    },
   };
 }
 
