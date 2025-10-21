ca14c883f93598fffb228cb1cd4d366c8ad66b24
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-10-21 23:19:19 +0600

    env set up for tokens

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,12 @@ dotenv.config();
 interface Env {
   PORT: number;
   NODE_ENV: string;
+  JWT: {
+    Access_Token_Secret: string;
+    Access_Token_Expires_In: string;
+    Refresh_Token_Secret: string;
+    Refresh_Token_Expires_In: string;
+  };
   HASH_SALT: number;
   Admin: {
     name: string;
@@ -20,6 +26,10 @@ function loadEnv(): Env {
     "ADMIN_EMAIL",
     "ADMIN_PASSWORD",
     "HASH_SALT",
+    "ACCESS_TOKEN_SECRET",
+    "ACCESS_TOKEN_EXPIRES_IN",
+    "REFRESH_TOKEN_SECRET",
+    "REFRESH_TOKEN_EXPIRES_IN",
   ];
   requiredVariables.forEach((variable) => {
     if (!process.env[variable]) {
@@ -37,6 +47,14 @@ function loadEnv(): Env {
       name: process.env.ADMIN_NAME as string,
       email: process.env.ADMIN_EMAIL as string,
       password: process.env.ADMIN_PASSWORD as string,
+    },
+
+    // JWT
+    JWT: {
+      Access_Token_Secret: process.env.ACCESS_TOKEN_SECRET as string,
+      Access_Token_Expires_In: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+      Refresh_Token_Secret: process.env.REFRESH_TOKEN_SECRET as string,
+      Refresh_Token_Expires_In: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
     },
   };
 }
