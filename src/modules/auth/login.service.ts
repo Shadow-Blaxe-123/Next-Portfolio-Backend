36d505d26128f05dd8b5ca34884cd61724f92bdd
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-10-22 21:04:13 +0600

    Added AppError utility

---
+++ src/modules/auth/login.service.ts
@@ -0,0 +1,22 @@
+import { StatusCodes } from "http-status-codes";
+import prisma from "../../config/prismaClient";
+import AppError from "../../error/AppError";
+import { compare } from "bcryptjs";
+import userTokens from "../../utils/userTokens";
+
+const login = async (email: string, password: string) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    throw new AppError(StatusCodes.NOT_FOUND, "User not found");
+  }
+  const isPasswordMatch = await compare(password, user.password);
+  if (!isPasswordMatch) {
+    throw new AppError(StatusCodes.UNAUTHORIZED, "Invalid password");
+  }
+
+  const accessToken = userTokens.createAccesstoken(user);
+  const refreshToken = userTokens.createRefreshToken(user);
+  return { accessToken, refreshToken };
+};
+
+export default login;
